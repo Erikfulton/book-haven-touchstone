0ab2d7fe92603ab88742d704a00f538af042fe03
Erikfulton <Erik_fulton@icloud.com>
2026-01-20 14:16:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function subscribeAlert() {
+  alert("Thank you for subscribing.");
+}
+
+function addToCartAlert() {
+  alert("Item added to the cart.");
+}
+
+function clearCartAlert() {
+  alert("Cart cleared.");
+}
+
+function processOrderAlert() {
+  alert("Thank you for your order.");
+}
+
+function contactFormAlert() {
+  alert("Thank you for your message.");
+}
